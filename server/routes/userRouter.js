25343daf0c7535be7f7eaae66da7340a78015006
farrellkai <farrellkai@gmail.com>
2024-04-25 13:49:15 -0400

    userRouter get request

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,4 +3,9 @@ const userController = require('../controllers/userController');
 
 const userRouter = express.Router();
 
+//retrieve user data from db
+userRouter.get('/', (req, res) => {
+  return res.status(200); //more will go here later
+});
+
 module.exports = userRouter;
