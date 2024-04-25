e47f726bab3a7d9d8da27223e058d491be20c780
farrellkai <farrellkai@gmail.com>
2024-04-25 13:28:59 -0400

    userRoutes path

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const userRoutes = require('./routes/userRoutes');
+
 const PORT = 3000;
 
 //parse request body
