a79cb116f649d7d441968ec59fb1c9177191efb8
farrellkai <farrellkai@gmail.com>
2024-04-25 14:01:10 -0400

    findUser function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,4 +2,7 @@ import db from '../models/entryModel';
 
 const userController = {};
 
+//find if username exists in db
+userController.findUser = async (req, res, next) => {};
+
 module.exports = userController;
