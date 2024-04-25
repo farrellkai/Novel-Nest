0acf55e3716f87c72d0d231cee054b2027ad27ab
farrellkai <farrellkai@gmail.com>
2024-04-25 13:24:12 -0400

    udefined routes error

--- server/server.js
+++ server/server.js
@@ -7,3 +7,8 @@ const PORT = 3000;
 //parse request body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//handles errors for any undefined route
+app.use('*', (req, res) => {
+  return res.status(404).send('Page not found');
+});
