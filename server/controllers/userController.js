f0af914cad2847961f215f9804f5775ce450fc48
farrellkai <farrellkai@gmail.com>
2024-04-26 12:59:29 -0400

    destructure req.body username

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,8 @@ import db from '../models/entryModel';
 const userController = {};
 
 //find if username exists in db
-userController.findUser = async (req, res, next) => {};
+userController.findUser = async (req, res, next) => {
+  const { username } = req.body;
+};
 
 module.exports = userController;
