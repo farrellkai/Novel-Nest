be44c7e7a8386541add361f9fba684b2902c5e06
farrellkai <farrellkai@gmail.com>
2024-04-26 13:10:35 -0400

    findUser query

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,6 +5,7 @@ const userController = {};
 //find if username exists in db
 userController.findUser = async (req, res, next) => {
   const { username } = req.body;
+  const query = `SELECT * FROM users WHERE username=${username}`;
 };
 
 module.exports = userController;
