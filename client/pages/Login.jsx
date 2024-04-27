fe65977d2b648cecdb9930fc9eb573771afe5fa2
farrellkai <farrellkai@gmail.com>
2024-04-27 15:09:02 -0400

    more inputs on login page

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-      <h1>'Login Page'</h1>
+      <h1>'Username'</h1>
+      <input type="text" />
+      <h1>'Password'</h1>
+      <input type="password" />
     </div>
   );
 };
