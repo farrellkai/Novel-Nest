44ff494840ef5173a73ca0f98f57362e2563e78d
farrellkai <farrellkai@gmail.com>
2024-04-27 15:20:33 -0400

    more element ids

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-      <p id="user">'Username'</p>
+      <p id="user">Username</p>
       <input type="text" name="username" id="username" />
-      <h1 id="pass">'Password'</h1>
+      <p id="pass">Password</p>
       <input type="password" name="password" id="password" />
       <button>Login</button>
     </div>
--- client/pages/Signup.jsx
+++ client/pages/Signup.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Signup = () => {
   return (
     <div>
-      <p>Username</p>
-      <input type="text" />
-      <p>Email</p>
-      <input type="text" />
-      <p>Password</p>
-      <input type="password" />
-      <p>Confirm Password</p>
-      <input type="password" />
+      <p id="user">Username</p>
+      <input type="text" name="username" id="username" />
+      <p id="mail">Email</p>
+      <input type="text" name="email" id="email" />
+      <p id="pass">Password</p>
+      <input type="password" name="password" id="password" />
+      <p id="conpass">Confirm Password</p>
+      <input type="password" name="conpassword" id="conpassword" />
       <button>Submit</button>
     </div>
   );
