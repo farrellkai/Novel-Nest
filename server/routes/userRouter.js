508bdb4bbe14effb12a085223755411a69977872
farrellkai <farrellkai@gmail.com>
2024-04-27 10:16:45 -0400

    varifyUser middleware in userRouter.get

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,9 +4,14 @@ const userController = require('../controllers/userController');
 const userRouter = express.Router();
 
 //retrieve user data from db
-userRouter.get('/', userController.findUser, (req, res) => {
-  return res.status(200); //more will go here later
-});
+userRouter.get(
+  '/',
+  userController.findUser,
+  userController.varifyUser,
+  (req, res) => {
+    return res.status(200); //more will go here later
+  }
+);
 
 //submit user data into db
 userRouter.post('/', userController.findUser, (req, res) => {
