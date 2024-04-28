45d391166faad06f38193f7595f038d6d57b9c60
farrellkai <farrellkai@gmail.com>
2024-04-28 14:07:41 -0400

    removed bugs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import MainContainer from '../containers/MainContainer';
 import { Route, Routes } from 'react-router-dom';
-
-//import pages
-import SplashPage from '../pages/SplashPage';
-import Login from '../pages/Login';
-import Signup from '../pages/Signup';
-
 import '../style.css';
 
 const App = () => {
