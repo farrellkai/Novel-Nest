0ac0e04369a1375a9967c8c5385a9255942cc622
farrellkai <farrellkai@gmail.com>
2024-04-28 13:43:42 -0400

    conditional rendering logged in

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -4,18 +4,20 @@ import Sidebar from '../components/Sidebar';
 
 const MainContainer = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-  return (
-    <div>
-      <div className="sidebar">
-        <Sidebar />
+  if (loggedIn) {
+    return (
+      <div>
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        {/* <Routes>
+          <Route path="/" element={<SplashPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes> */}
       </div>
-      {/* <Routes>
-        <Route path="/" element={<SplashPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes> */}
-    </div>
-  );
+    );
+  }
 };
 
 export default MainContainer;
