1bac3cb23586190b28102550ad0e7cb4d1881d1a
farrellkai <farrellkai@gmail.com>
2024-04-28 13:48:15 -0400

    more logging in logic

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -5,6 +5,30 @@ import Sidebar from '../components/Sidebar';
 const MainContainer = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [hasAccount, setHasAccount] = useState(true);
+
+  const navigate = useNavigate();
+  //logic to verify username and password are valid
+  const verifyUser = async () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    try {
+      const response = await fetch('api/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username, password: password }),
+      });
+
+      if (!response.ok) throw new Error('Incorrect username or password');
+      else if (response.ok) {
+        setLoggedIn(true);
+      }
+    } catch (err) {
+      console.log('Error:', err);
+    }
+  };
+
   if (loggedIn) {
     return (
       <div>
@@ -16,6 +40,20 @@ const MainContainer = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes> */}
+      </div>
+    );
+  } else if (!loggedIn && hasAccount) {
+    return (
+      <div id="wrapper">
+        <div id="login">
+          <p id="user">Username</p>
+          <input type="text" name="username" id="username" />
+          <p id="pass">Password</p>
+          <input type="password" name="password" id="password" />
+          <button className="loginButton" id="loginButton" onClick={verifyUser}>
+            Login
+          </button>
+        </div>
       </div>
     );
   }
