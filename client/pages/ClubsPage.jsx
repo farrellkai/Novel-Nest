e583444670890330d9acc06fed908916d9015abe
farrellkai <farrellkai@gmail.com>
2024-04-29 13:52:24 -0400

    extablished clubs page

---
+++ client/pages/ClubsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style.css';
+
+const ClubsPage = (props) => {
+  const { loggedIn, user } = props;
+  return (
+    <div id="clubsPage">
+      <h1>This is the clubs page</h1>
+    </div>
+  );
+};
+
+export default ClubsPage;
