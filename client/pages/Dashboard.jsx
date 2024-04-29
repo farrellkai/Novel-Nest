777bb0af96e69ee20bc41f40dcfd02cba19aa885
farrellkai <farrellkai@gmail.com>
2024-04-29 13:31:11 -0400

    general cleaning

--- client/pages/Dashboard.jsx
+++ client/pages/Dashboard.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import '../style.css';
 
 const Dashboard = (props) => {
-  const { loggedIn } = props;
-  console.log('THIS IS THE STATE, BRO!!!:', loggedIn);
+  const { loggedIn, user } = props;
   return (
     <div>
       <h1>This is the dashboard</h1>
