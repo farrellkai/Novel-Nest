c40a19574659c85c923cfd7b5444f05c5fa46e06
farrellkai <farrellkai@gmail.com>
2024-04-29 13:48:35 -0400

    sidebar link to books page

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div>
-      <h1>This is the sidebar</h1>
+      <Link to="/books" className="books" id="books">
+        Your Books
+      </Link>
     </div>
   );
 };
