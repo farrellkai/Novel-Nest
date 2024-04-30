19cb7ebbd7863d3e286f617595cbf37734db23c0
farrellkai <farrellkai@gmail.com>
2024-04-30 14:27:17 -0400

    getTitle middleware function

--- server/controllers/externalController.js
+++ server/controllers/externalController.js
@@ -2,4 +2,7 @@ const db = require('../models/entryModel');
 
 const externalController = {};
 
+//retrieve book data by title
+externalController.getTitle = async () => {};
+
 module.exports = externalController;
