b30e6ea29e777f4456614c02ae7e948772221e44
farrellkai <farrellkai@gmail.com>
2024-04-30 14:38:53 -0400

    parsing data

--- server/controllers/externalController.js
+++ server/controllers/externalController.js
@@ -13,6 +13,8 @@ externalController.getTitle = async () => {
     const response = await fetch(
       `https://www.googleapis.com/books/v1/volumes?q=${title}&key=${API_KEY}`
     );
+    const data = await response.json();
+    console.log(data);
   } catch (err) {}
 };
 
