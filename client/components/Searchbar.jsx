e8c955dd1b2ed2c78410df355c585bdbede39fd8
farrellkai <farrellkai@gmail.com>
2024-05-01 14:03:52 -0400

    searching hook

--- client/components/Searchbar.jsx
+++ client/components/Searchbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Searchbar = () => {
+  const [searching, setSearching] = useState(false);
   return (
     <div>
       <input id="searchbar" type="text" />
