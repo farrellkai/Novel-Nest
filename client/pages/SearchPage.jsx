f2c66e73c4d893ed32ae59e0ff02979745230420
farrellkai <farrellkai@gmail.com>
2024-05-02 16:49:40 -0400

    experimenting with fetching image data

--- client/pages/SearchPage.jsx
+++ client/pages/SearchPage.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 const SearchPage = (props) => {
   console.log(props);
+  return (
+    <div class="dropdown">
+      <button class="dropbtn">Dropdown</button>
+      <div class="dropdown-content">
+        <a>Want to Read</a>
+        <a>Currently Reading</a>
+        <a>Read</a>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
