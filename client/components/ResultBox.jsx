45e3c576ac109dbf7fce9cf1fb3600960afaaf92
farrellkai <farrellkai@gmail.com>
2024-05-02 15:16:06 -0400

    book data passed to SearchPage

--- client/components/ResultBox.jsx
+++ client/components/ResultBox.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import SearchModal from './modals/SearchModal';
 
 const ResultBox = (props) => {
   const { book, getSearchData } = props;
-  console.log(book.volumeInfo);
   return (
     <Link to="/search" className="page" id="searchPage">
       <div className="resultBox" onClick={() => getSearchData(book)}>
