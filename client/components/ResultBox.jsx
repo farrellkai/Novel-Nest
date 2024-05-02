67ecccfba671c37d6fa8b98401d257b3b91fcfc3
farrellkai <farrellkai@gmail.com>
2024-05-02 15:07:13 -0400

    redfining classNames

--- client/components/ResultBox.jsx
+++ client/components/ResultBox.jsx
@@ -2,17 +2,10 @@ import React, { useEffect, useState } from 'react';
 import SearchModal from './modals/SearchModal';
 
 const ResultBox = (props) => {
-  const [searchModal, setSearchModal] = useState(false);
   const { book, getSearchData } = props;
   console.log(book.volumeInfo);
   return (
-    <div
-      className="resultBox"
-      onClick={() => {
-        setSearchModal(true);
-        getSearchData(book);
-      }}
-    >
+    <div className="resultBox" onClick={() => getSearchData(book)}>
       <p>{book.volumeInfo.title}</p>
       <p>{book.volumeInfo.authors}</p>
       {searchModal && <SearchModal />}
--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <div id="links">
-      <Link to="/" className="dashboard" id="dashboard">
+      <Link to="/" className="page" id="dashboard">
         Dashboard
       </Link>
-      <Link to="/books" className="books" id="books">
+      <Link to="/books" className="page" id="books">
         Your Books
       </Link>
-      <Link to="/clubs" className="clubs" id="clubs">
+      <Link to="/clubs" className="page" id="clubs">
         Your Book Clubs
       </Link>
     </div>
