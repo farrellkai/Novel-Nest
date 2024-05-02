46b2736f9af7767eed17c721ee9559befb5fbbd8
farrellkai <farrellkai@gmail.com>
2024-05-02 13:15:26 -0400

    destructuring book from props

--- client/components/ResultBox.jsx
+++ client/components/ResultBox.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 
 const ResultBox = (props) => {
+  const { book } = props;
+  console.log(props);
   return (
     <div>
+      <p>{props.book.volumeInfo.title}</p>
       <p>{props.book.volumeInfo.title}</p>
     </div>
   );
