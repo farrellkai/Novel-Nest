9db3ab9791b5d02119a165225bde7c48ffcd5aca
farrellkai <farrellkai@gmail.com>
2024-05-02 15:04:36 -0400

    setting getSearchData

--- client/components/ResultBox.jsx
+++ client/components/ResultBox.jsx
@@ -3,10 +3,16 @@ import SearchModal from './modals/SearchModal';
 
 const ResultBox = (props) => {
   const [searchModal, setSearchModal] = useState(false);
-  const { book } = props;
+  const { book, getSearchData } = props;
   console.log(book.volumeInfo);
   return (
-    <div className="resultBox" onClick={() => setSearchModal(true)}>
+    <div
+      className="resultBox"
+      onClick={() => {
+        setSearchModal(true);
+        getSearchData(book);
+      }}
+    >
       <p>{book.volumeInfo.title}</p>
       <p>{book.volumeInfo.authors}</p>
       {searchModal && <SearchModal />}
