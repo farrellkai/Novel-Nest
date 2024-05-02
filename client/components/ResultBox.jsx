575a2c1b5bad7c1b2ea08d0d3f8d67f68b73d6ce
farrellkai <farrellkai@gmail.com>
2024-05-02 15:09:25 -0400

    link to searchPage

--- client/components/ResultBox.jsx
+++ client/components/ResultBox.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import SearchModal from './modals/SearchModal';
 
 const ResultBox = (props) => {
   const { book, getSearchData } = props;
   console.log(book.volumeInfo);
   return (
-    <div className="resultBox" onClick={() => getSearchData(book)}>
-      <p>{book.volumeInfo.title}</p>
-      <p>{book.volumeInfo.authors}</p>
-      {searchModal && <SearchModal />}
-    </div>
+    <Link to="/search" className="page" id="searchPage">
+      <div className="resultBox" onClick={() => getSearchData(book)}>
+        <p>{book.volumeInfo.title}</p>
+        <p>{book.volumeInfo.authors}</p>
+      </div>
+    </Link>
   );
 };
 
