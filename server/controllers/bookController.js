0f6271f3f2fa6a750bb5be058c0321887ee4fb65
farrellkai <farrellkai@gmail.com>
2024-05-03 12:20:57 -0400

    try/catch in findBook middleware

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -5,6 +5,8 @@ const bookController = {};
 bookController.findBook = async (req, res, next) => {
   const { title, author } = req.body;
   const query = 'SELECT _id FROM books WHERE title=$1 AND author=$2';
+  try {
+  } catch (err) {}
 };
 
 module.exports = bookController;
