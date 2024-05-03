41864685a533b21092fec56a9f4b0032f44ad8ec
farrellkai <farrellkai@gmail.com>
2024-05-03 12:59:32 -0400

    if book was already found return next middleware function

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -2,6 +2,7 @@ const db = require('../models/entryModel');
 
 const bookController = {};
 
+//find book in db with matching title and author and pass object to next middleware function
 bookController.findBook = async (req, res, next) => {
   const { title, author } = req.body;
   const query = 'SELECT _id FROM books WHERE title=$1 AND author=$2';
@@ -16,6 +17,11 @@ bookController.findBook = async (req, res, next) => {
       message: { err: 'cannot find book' },
     });
   }
+};
+
+//if book was not found in previous middleware function insert its data to db
+bookController.addBook = async (req, res, next) => {
+  if (res.locals.bookID) return next();
 };
 
 module.exports = bookController;
