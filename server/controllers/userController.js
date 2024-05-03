aee69f8346810cedbb40126caa911c8f9dac057c
farrellkai <farrellkai@gmail.com>
2024-05-03 13:52:24 -0400

    get or post request conditions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,8 +6,13 @@ const userController = {};
 //find if username exists in db
 userController.findUser = async (req, res, next) => {
   console.log('***findUser middleware running***');
-  //pull username from req.body
-  const { username } = req.body;
+  //conditions regarding if its handling get or post request
+  if (req.body) {
+    //pull username from req.body
+    const { username } = req.body;
+  } else if (req.params) {
+    const { username } = req.params;
+  }
   //find row with matching username in db
   const query = 'SELECT * FROM users WHERE username=$1';
   try {
