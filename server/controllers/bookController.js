bb74219701452b61508bfd07ea72eaac690b4d7a
farrellkai <farrellkai@gmail.com>
2024-05-03 17:16:16 -0400

    addUserBook execute query

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -59,6 +59,7 @@ bookController.addUserBook = async (req, res, next) => {
   const query =
     'INSERT INTO user_books (user_id, book_id, status, started) VALUES ($1, $2, $3, $4)';
   try {
+    await db.query(query, [userID, _id, status, date]);
   } catch (err) {}
 };
 
