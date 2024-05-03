c0a9e45b74083a00958a3d03cb091d7a4389fec7
farrellkai <farrellkai@gmail.com>
2024-05-03 12:27:17 -0400

    return status 200

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -3,6 +3,8 @@ const bookController = require('../controllers/bookController');
 
 const bookRouter = express.Router();
 
-bookRouter.post('/', bookController.findBook, (req, res) => {});
+bookRouter.post('/', bookController.findBook, (req, res) => {
+  return res.sendStatus(200);
+});
 
 module.exports = bookRouter;
