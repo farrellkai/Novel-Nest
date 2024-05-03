946b4e4071ea799f16883a9643339c0aee4e5135
farrellkai <farrellkai@gmail.com>
2024-05-03 13:25:02 -0400

    addBook middleware in bookRouter

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -3,8 +3,13 @@ const bookController = require('../controllers/bookController');
 
 const bookRouter = express.Router();
 
-bookRouter.post('/', bookController.findBook, (req, res) => {
-  return res.sendStatus(200);
-});
+bookRouter.post(
+  '/',
+  bookController.findBook,
+  bookController.addBook,
+  (req, res) => {
+    return res.sendStatus(200);
+  }
+);
 
 module.exports = bookRouter;
