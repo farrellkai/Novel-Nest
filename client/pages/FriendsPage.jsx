1b27aa5e45fb8900421ac150a768bb5a9a9976b9
farrellkai <farrellkai@gmail.com>
2024-05-13 14:13:24 -0400

    FriendsPage rendering

--- client/pages/FriendsPage.jsx
+++ client/pages/FriendsPage.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const FriendsPage = () => {};
+const FriendsPage = () => {
+  return (
+    <div id="friendsPage">
+      <h1>This is the friends page</h1>
+    </div>
+  );
+};
 
 export default FriendsPage;
