b9691315b24b6d61ba523e8034552951bd0fdf52
farrellkai <farrellkai@gmail.com>
2024-05-14 14:14:41 -0400

    BooksPage button

--- client/pages/BooksPage.jsx
+++ client/pages/BooksPage.jsx
@@ -7,12 +7,14 @@ const BooksPage = (props) => {
   const getUserBooks = async () => {
     try {
       const response = await fetch(`/api/book/all-books/${userID}`);
+      console.log(response);
     } catch (err) {}
   };
 
   return (
     <div id="booksPage">
       <h1>This is the books page</h1>
+      <button onClick={() => getUserBooks()}>get book data</button>
     </div>
   );
 };
