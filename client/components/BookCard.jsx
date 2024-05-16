9cff71e7256c8b6cbdd230088251cf5fb367f8ab
farrellkai <farrellkai@gmail.com>
2024-05-16 15:02:53 -0400

    BookCard properly rendering title

--- client/components/BookCard.jsx
+++ client/components/BookCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BookCard = ({ bookData }) => {
   const { title } = bookData;
-  return <h1>This is a book</h1>;
+  return <h1>{title}</h1>;
 };
 
 export default BookCard;
