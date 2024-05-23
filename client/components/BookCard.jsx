52054a51c5aedd999e38253f2071074bb21e1a16
farrellkai <farrellkai@gmail.com>
2024-05-23 11:27:29 -0400

    render authors

--- client/components/BookCard.jsx
+++ client/components/BookCard.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const BookCard = ({ bookData }) => {
   const { title, authors } = bookData;
-  return <h1>{title}</h1>;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h3>by {authors}</h3>
+    </div>
+  );
 };
 
 export default BookCard;
